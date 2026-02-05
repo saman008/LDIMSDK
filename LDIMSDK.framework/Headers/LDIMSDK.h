a4605ed8fbaf5a057494681959f7bc52a56120d8
zhaokunpeng <1975566197@qq.com>
2026-02-05 14:21:37 +0800

    提交podspec

---
+++ LDIMSDK.framework/Headers/LDIMSDK.h
@@ -0,0 +1,20 @@
+//
+//  LDIMSDK.h
+//  LDIMSDK
+//
+//  Created by youliaobf on 2026/2/4.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CMSDK.
+FOUNDATION_EXPORT double LDIMSDKVersionNumber;
+
+//! Project version string for CMSDK.
+FOUNDATION_EXPORT const unsigned char LDIMSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CMSDK/PublicHeader.h>
+
+#import <RxSwift/RxSwift.h>
+#import <SnapKit/SnapKit.h>
+
---
+++ LDIMSDK.framework/Headers/LDIMSDK.swift
@@ -0,0 +1,67 @@
+//
+//  LDIMSDK.swift
+//  LDIMSDK
+//
+//  Created by youliaobf on 2026/2/4.
+//
+
+import Foundation
+
+@_exported import RxSwift
+@_exported import RxCocoa
+@_exported import RxDataSources
+@_exported import RxGesture
+@_exported import SnapKit
+
+open class LDIMSDK: NSObject {
+    
+    public static func getBundleImage(target: UIViewController) {
+        let img = UIImage.bundledImage(named: "icon-20")
+        let img1 = UIImage.bundledImage(named: "icon-20.png")
+        let img2 = UIImage.bundledImage(named: "icon-30")
+        let img3 = UIImage.bundledImage(named: "icon-30.png")
+        let img4 = UIImage.bundledImage(named: "icon-40")
+        let img5 = UIImage.bundledImage(named: "icon-40.png")
+        print(img)
+        print(img1)
+        print(img2)
+        print(img3)
+        print(img4)
+        print(img5)
+        let imageview1 = UIImageView(image: img)
+        imageview1.frame = CGRect(x: 100, y: 100, width: 100, height: 100)
+        let imageview2 = UIImageView(image: img2)
+        imageview2.frame = CGRect(x: 100, y: 200, width: 100, height: 100)
+
+        let imageview3 = UIImageView(image: img4)
+        imageview1.frame = CGRect(x: 100, y: 300, width: 100, height: 100)
+
+        target.view.addSubview(imageview1)
+        target.view.addSubview(imageview2)
+        target.view.addSubview(imageview3)
+        
+    }
+    public static func shopPick(target: UIViewController,choiceArray:[String], title: String = "请选择",finishedCallback : @escaping (_ index:Int,_ json : String) -> ()){
+        
+        let alert = UIAlertController(title: nil, message: title, preferredStyle: .actionSheet)
+        let cancelAction = UIAlertAction(title: "取消", style: .cancel, handler: nil)
+        for (id,i) in choiceArray.enumerated(){
+            let okAction = UIAlertAction(title: i, style: .default, handler: { (action:UIAlertAction) -> Void in
+                finishedCallback(id, i)
+                
+            })
+            alert.addAction(okAction)
+        }
+        
+        
+        alert.addAction(cancelAction)
+        target.present(alert, animated: true, completion: nil)
+        
+    }
+    
+    public static func getME() -> UIViewController{
+
+        return LDAboutMeAndContactMeViewController()
+    }
+    
+}
